ba2925dd6a4e1a396e5e10144083b85aa2dadf91
heydarov93 <heyderov.yashar@mail.ru>
2024-01-10 06:47:32 -0800

    push function created. adds new value to the stack

--- _push.c
+++ _push.c
@@ -1,8 +1,25 @@
 #include "monty.h"
 
+/**
+ * push - prints all stack
+ * @stack: pointer to the pointer to the stack
+ * @line_number: the line number currently being read
+ *
+ */
+
 void push(stack_t **stack, unsigned int line_number)
 {
-	(void) stack;
+	stack_t *new_stack = (stack_t *)malloc(sizeof(stack_t));
 	(void) line_number;
-	printf("push function\n");
+
+	if (new_stack == NULL)
+	{
+		free(new_stack);
+		return;
+	}
+
+	new_stack->n = global.int_value;
+	new_stack->prev = NULL;
+	new_stack->next = *stack;
+	*stack = new_stack;
 }

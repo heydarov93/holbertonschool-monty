8de21923e1ce60ec487669da9955797f5cf0e7e8
heydarov93 <heyderov.yashar@mail.ru>
2024-01-10 06:51:42 -0800

    free_all_stack function created

--- main.c
+++ main.c
@@ -10,31 +10,41 @@
 
 int main(int argc, char **argv)
 {
-	stack_t *ptr_stack, stack;
-	//instruction_t instructions[7] = { {"push", push} }
-
-	ptr_stack = &stack;
-
 	if (argc != 2)
 	{
 		fprintf(stderr, RED "USAGE: monty file\n" COLOR_RESET);
-		exit (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 
 	init_global();
-
-	ptr_stack->n = global.int_value;
-	ptr_stack->prev = NULL;
-	ptr_stack->next = NULL;
 
 	global.file = fopen(argv[1], "r");
 
 	if (!global.file)
 		error(2, "Error: Can't open file ", argv[1]);
 
-	_read_file();
+	if (_read_execute() == -1)
+		exit(EXIT_FAILURE);
 
 	fclose(global.file);
+	free_all_stack();
 
 	return (0);
+}
+
+/**
+ * free_all_stack - frees all stack
+ *
+ */
+
+void free_all_stack(void)
+{
+	stack_t *tmp_stack = global.stack;
+
+	while (global.stack)
+	{
+		tmp_stack = global.stack->next;
+		free(global.stack);
+		global.stack = tmp_stack;
+	}
 }

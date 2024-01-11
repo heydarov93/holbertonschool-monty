6d86da7c87392c3d801ead5818979ff087c4096a
heydarov93 <heyderov.yashar@mail.ru>
2024-01-11 08:35:57 -0800

    error messages added according to error codes

--- _print_error.c
+++ _print_error.c
@@ -9,22 +9,36 @@
 
 void print_error(int err_code)
 {
-	switch (err_code)
-	{
-		case 400:
-			fprintf(stderr,
-				"L<%u>: usage: push integer\n",
+switch (err_code)
+{
+	case 204:
+		fprintf(stderr,	"L<%u>: can't pint, stack empty\n",
 				global.line);
-			break;
-		case 404:
-			fprintf(stderr,
-				"L<%u>: unknown instruction <%s>\n",
+		break;
+	case 205:
+		fprintf(stderr,	"L<%u>: can't pop an empty stack\n",
+				global.line);
+		break;
+	case 206:
+		fprintf(stderr,	"L<%u>: can't swap, stack too short\n",
+				global.line);
+		break;
+	case 207:
+		fprintf(stderr,	"L<%u>: can't add, stack too short\n",
+				global.line);
+		break;
+	case 400:
+		fprintf(stderr,	"L<%u>: usage: push integer\n",
+				global.line);
+		break;
+	case 404:
+		fprintf(stderr,	"L<%u>: unknown instruction <%s>\n",
 				global.line, global.opcode);
-			break;
-		case 500:
-			fprintf(stderr, "Error: malloc failed\n");
-			break;
-	}
+		break;
+	case 500:
+		fprintf(stderr, "Error: malloc failed\n");
+		break;
+}
 
 	free(global.opcode);
 	fclose(global.file);

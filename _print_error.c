6e655a340f008fbe7948e0c1ddd43591ba4d98f4
heydarov93 <heyderov.yashar@mail.ru>
2024-01-11 20:52:04 -0800

    <> signs removed from messages

--- _print_error.c
+++ _print_error.c
@@ -12,27 +12,27 @@ void print_error(int err_code)
 switch (err_code)
 {
 	case 204:
-		fprintf(stderr,	"L<%u>: can't pint, stack empty\n",
+		fprintf(stderr,	"L%u: can't pint, stack empty\n",
 				global.line);
 		break;
 	case 205:
-		fprintf(stderr,	"L<%u>: can't pop an empty stack\n",
+		fprintf(stderr,	"L%u: can't pop an empty stack\n",
 				global.line);
 		break;
 	case 206:
-		fprintf(stderr,	"L<%u>: can't swap, stack too short\n",
+		fprintf(stderr,	"L%u: can't swap, stack too short\n",
 				global.line);
 		break;
 	case 207:
-		fprintf(stderr,	"L<%u>: can't add, stack too short\n",
+		fprintf(stderr,	"L%u: can't add, stack too short\n",
 				global.line);
 		break;
 	case 400:
-		fprintf(stderr,	"L<%u>: usage: push integer\n",
+		fprintf(stderr,	"L%u: usage: push integer\n",
 				global.line);
 		break;
 	case 404:
-		fprintf(stderr,	"L<%u>: unknown instruction <%s>\n",
+		fprintf(stderr,	"L%u: unknown instruction %s\n",
 				global.line, global.opcode);
 		break;
 	case 500:
